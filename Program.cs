036609fdba3cee673443eefb09a1c731e1c19db3
gorgleb <98337705+gorgleb@users.noreply.github.com>
2022-02-25 20:59:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+namespace CodeWars
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        { 
+        
+            int n=19;
+            Console.WriteLine(Parser.HumanTimeFormat.NSquaresFor(n));
+
+        }
+
+
+    }
+
+
+    public class Parser
+    {
+        public class HumanTimeFormat
+        {
+            public static int NSquaresFor(int n)
+            {
+                int counter = 0;
+                double number = (double)n;
+                while (n > 0)
+                {
+                    
+                    if ((Math.Sqrt(number)) % 1 == 0)
+                    {
+                        counter++;
+                        n -= (int)number;
+                        number = n;
+                    }
+                    else
+                    {
+                        number--;
+                    }
+                }
+                
+                   
+               
+                    
+                
+                return counter;
+            }
+
+          
+        }
+    }
+
+
+}
+
+
